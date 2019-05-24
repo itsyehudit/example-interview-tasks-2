c259fa6cfced3427267267e49309f9edb39d12a0
Katarzyna Mildner <itsyehudit@gmail.com>
2019-05-24 23:38:16 +0200

    FizzBuzz - single parameter: new feature - when float is given, user can choose to round the input

--- FizzBuzz - single parameter.rb
+++ FizzBuzz - single parameter.rb
@@ -1,16 +1,32 @@
 def fizzBuzz(isFB)
-  if
-    isFB%3 === 0 && isFB%5 === 0
-    puts "FizzBuzz"
-  elsif isFB%3 === 0
-    puts "Fizz"
-  elsif isFB%5 === 0
-    puts "Buzz"
+
+  def printFizzBuzz(isFB)
+    if
+      isFB%3 === 0 && isFB%5 === 0
+      puts "FizzBuzz"
+    elsif isFB%3 === 0
+      puts "Fizz"
+    elsif isFB%5 === 0
+      puts "Buzz"
+    else
+      puts "No FizzBuz for you, sport!"
+    end
+  end
+
+  if isFB%1 === 0
+    printFizzBuzz(isFB)
   else
-    puts "No FizzBuz for you, sport!"
+    puts "Would you like to round your number #{isFB}? Y/N"
+    q = gets.chomp
+    if q === "y" || q === "Y"
+      isFB = isFB.round
+      printFizzBuzz(isFB)
+    else
+      printFizzBuzz(isFB)
+    end
   end
 end
 
 puts "Pick a number:"
 
-fizzBuzz(gets.to_i)
+fizzBuzz(gets.to_f)
