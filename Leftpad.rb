502428e39736f43001620ed2a6ff4ff5cd95120e
Katarzyna Mildner <itsyehudit@gmail.com>
2019-07-08 17:38:47 +0200

    Refacotr: wrapped LeftPad logic into a Class

--- Leftpad.rb
+++ Leftpad.rb
@@ -1,37 +1,41 @@
-def left_pad(string = "You are extremely lazy, Kid", times = 1, char = " ")
-  puts "Choose a word:"
-  string_in = gets.chomp
-  puts "Choose a number:"
-  times_in = gets.to_i
-  puts "Choose a character:"
-  char_in = gets.chomp
-  puts "Would you like to have your word of choice before all the other characters? Y/N"
-  bool = gets.chomp
-  bool.downcase!
+class Left_Pad
+  def left_pad(string, times, char, bool)
+    chars = char * times
 
-  if string_in == ""
-    string_in = string
-  end
+    if char.length > 1
+      taunt = "I\'ve asked you for *a* character, but ok:\n"
+    end
 
-  if times_in == ""
-    times_in = times
-  end
-
-  if char_in == ""
-    char_in = char
-  end
-
-  chars = char_in * times_in
-
-  if char_in.length > 1
-    puts "I\'ve asked you for *a* character, but ok:"
-  end
-
-  if bool == "y"
-    puts "Your word&characters coctail is: #{string_in}#{chars}."
-  else
-    puts "Your characters&word coctail is: #{chars}#{string_in}."
+    if bool == "y"
+      "#{taunt}Your word&characters coctail is: #{string}#{chars}."
+    else
+      "#{taunt}Your characters&word coctail is: #{chars}#{string}."
+    end
   end
 end
 
-left_pad
+lp_case = Left_Pad.new
+
+puts "Choose a word:"
+string_in = gets.chomp
+puts "Choose a number:"
+times_in = gets.to_i
+puts "Choose a character:"
+char_in = gets.chomp
+puts "Would you like to have your word of choice before all the other characters? Y/N"
+bool_in = gets.chomp
+bool_in.downcase!
+
+if string_in == ""
+  string_in = "You are extremely lazy, Kid"
+end
+
+if times_in == ""
+  times_in = 1
+end
+
+if char_in == ""
+  char_in = " "
+end
+
+puts lp_case.left_pad(string_in, times_in, char_in, bool_in)
