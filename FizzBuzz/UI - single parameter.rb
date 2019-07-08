434bb200e2f29f35b88181643a520db023c06a23
Katarzyna Mildner <itsyehudit@gmail.com>
2019-07-08 22:36:46 +0200

    Refactor: wrapped single parameter UI into a Class

--- FizzBuzz/UI - single parameter.rb
+++ FizzBuzz/UI - single parameter.rb
@@ -1,34 +1,42 @@
 require_relative 'logic'
 
-fb_case = Fizz_Buzz.new
+class UI_single_parameter
+  def initialize
+    @fb_case = Fizz_Buzz.new
+  end
 
-puts "Pick a number:"
-input = gets.chomp
-input = input.gsub(/,/, '.')
+  def fizz_buzz_input
+    puts "Pick a number:"
+    input = gets.chomp
+    input = input.gsub(/,/, '.')
 
-begin
-  input = Float(input)
-rescue ArgumentError => e
-  puts "Improper value: #{e.class.name}, try again."
-  fb_case.print_fizz_buzz(input)
-  exit
-end
-
-if input%1 == 0
-else
-  puts "Would you like to round your number #{input}? Y/N"
-  q = gets.chomp.downcase
-  if q == "y"
-    puts "Would you like to round your number #{input} UP? Y/N"
-    updown = gets.chomp.downcase
-    if updown == "y"
-      puts "Your number #{input} will be rounded UP."
-      input = input.ceil
-    else
-      puts "Your number #{input} will be rounded DOWN."
-      input = input.floor
+    begin
+      input = Float(input)
+    rescue ArgumentError => e
+      puts "Improper value: #{e.class.name}, try again."
+      @fb_case.print_fizz_buzz(input)
+      exit
     end
+
+    if input%1 == 0
+    else
+      puts "Would you like to round your number #{input}? Y/N"
+      q = gets.chomp.downcase
+      if q == "y"
+        puts "Would you like to round your number #{input} UP? Y/N"
+        updown = gets.chomp.downcase
+        if updown == "y"
+          puts "Your number #{input} will be rounded UP."
+          input = input.ceil
+        else
+          puts "Your number #{input} will be rounded DOWN."
+          input = input.floor
+        end
+      end
+    end
+    puts @fb_case.print_fizz_buzz(input)
   end
 end
 
-puts fb_case.print_fizz_buzz(input)
+fizz_buzz = UI_single_parameter.new
+fizz_buzz.fizz_buzz_input
