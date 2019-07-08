c88906f3389b6edc47ac137b9fe45a6050b30b42
Katarzyna Mildner <itsyehudit@gmail.com>
2019-07-08 17:42:53 +0200

    Refactor: separated logic and UI into two files

--- LeftPad_UI.rb
+++ LeftPad_UI.rb
@@ -1,18 +1,4 @@
-class Left_Pad
-  def left_pad(string, times, char, bool)
-    chars = char * times
-
-    if char.length > 1
-      taunt = "I\'ve asked you for *a* character, but ok:\n"
-    end
-
-    if bool == "y"
-      "#{taunt}Your word&characters coctail is: #{string}#{chars}."
-    else
-      "#{taunt}Your characters&word coctail is: #{chars}#{string}."
-    end
-  end
-end
+require_relative 'LeftPad_logic'
 
 lp_case = Left_Pad.new
 
---
+++ LeftPad_logic.rb
@@ -0,0 +1,15 @@
+class Left_Pad
+  def left_pad(string, times, char, bool)
+    chars = char * times
+
+    if char.length > 1
+      taunt = "I\'ve asked you for *a* character, but ok:\n"
+    end
+
+    if bool == "y"
+      "#{taunt}Your word&characters coctail is: #{string}#{chars}."
+    else
+      "#{taunt}Your characters&word coctail is: #{chars}#{string}."
+    end
+  end
+end
