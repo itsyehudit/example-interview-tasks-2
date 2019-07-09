c6f896dc956b218359cd83cc0f6b321c1d3d1cdf
Katarzyna Mildner <itsyehudit@gmail.com>
2019-07-09 19:01:06 +0200

    Refactor: wrapped LeftPad UI into a class

--- LeftPad/UI.rb
+++ LeftPad/UI.rb
@@ -1,27 +1,36 @@
 require_relative 'logic'
 
-lp_case = Left_Pad.new
+class UI_Left_Pad
+  def initialize
+    @lp_case = Left_Pad.new
+  end
 
-puts "Choose a word:"
-string_in = gets.chomp
-puts "Choose a number:"
-times_in = gets.to_i
-puts "Choose a character:"
-char_in = gets.chomp
-puts "Would you like to have your word of choice before all the other characters? Y/N"
-bool_in = gets.chomp
-bool_in.downcase!
+  def left_pad_input
+    puts "Choose a word:"
+    string_in = gets.chomp
+    puts "Choose a number:"
+    times_in = gets.to_i
+    puts "Choose a character:"
+    char_in = gets.chomp
+    puts "Would you like to have your word of choice before all the other characters? Y/N"
+    bool_in = gets.chomp
+    bool_in.downcase!
 
-if string_in == ""
-  string_in = "You are extremely lazy, Kid"
+    if string_in == ""
+      string_in = "You are extremely lazy, Kid"
+    end
+
+    if times_in == ""
+      times_in = 1
+    end
+
+    if char_in == ""
+      char_in = " "
+    end
+
+    puts @lp_case.left_pad(string_in, times_in, char_in, bool_in)
+  end
 end
 
-if times_in == ""
-  times_in = 1
-end
-
-if char_in == ""
-  char_in = " "
-end
-
-puts lp_case.left_pad(string_in, times_in, char_in, bool_in)
+left_pad = UI_Left_Pad.new
+left_pad.left_pad_input
